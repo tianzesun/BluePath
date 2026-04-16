1b4f33c6d1f2872db69a7ee3b7404c873dd99ba3
Tianze Sun <tianzesun@users.noreply.github.com>
2026-04-16 16:34:51 -0400

    Create next.config.ts

---
+++ apps/web/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['uoft.me', 'utsc.utoronto.ca'], // Add university domains here
+  },
+};
+
+export default nextConfig;
